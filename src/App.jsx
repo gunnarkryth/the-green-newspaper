c20201d7e6956f59a68c530944c817e9664a33f5
Gunski <gunnarkryth@gmail.com>
2025-03-10 09:29:55 +0100

    Button styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-function App() {
+import { Button } from "./components/Button";
 
+function App() {
   return (
     <>
-      
+      <Button backgroundColor={"interactive"} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,16 @@
+import s from "./Button.module.scss";
+
 export const Button = ({ backgroundColor, border, text, icon }) => {
   return (
-    <button style={{ backgroundColor: backgroundColor, border: border }}>
-      {icon ? <img src={icon} alt={icon} /> : <p>{text}</p>}
+    <button
+      className={`${s.button} ${s[backgroundColor]}`}
+      style={{ backgroundColor: backgroundColor, border: border }}
+    >
+      {icon ? (
+        <img className={s.icon} src={icon} alt={icon} />
+      ) : (
+        <p className={s.text}>{text}</p>
+      )}
     </button>
   );
 };
