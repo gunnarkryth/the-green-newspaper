f362da611ff942dcca428bea3e6d06f02332edb4
Gunski <gunnarkryth@gmail.com>
2025-03-12 09:22:44 +0100

    Product page not working

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,14 +1,17 @@
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 import s from "./ProductCard.module.scss";
 
 export const ProductCard = ({ image, name, children, slug }) => {
+  const navigate = useNavigate();
+
   return (
-    <NavLink to={`/product/${slug}`}>
-      <figure className={s.productCard}>
-        <img src={image} alt={`image of ${name}`} />
-        <figcaption className={s.popUp}>{name}</figcaption>
-        {children}
-      </figure>
-    </NavLink>
+    <figure
+      className={s.productCard}
+      onClick={() => navigate(`/product/${slug}`)}
+    >
+      <img src={image} alt={`image of ${name}`} />
+      <figcaption className={s.popUp}>{name}</figcaption>
+      {children}
+    </figure>
   );
 };
